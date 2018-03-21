996ad5c91c34a626016e7aaf3c03850f0737054b
Johann Lee <me@qinka.pro>
2018-03-21 21:20:23 +0800

    update; testing done

--- libvts/kernel/scale.cl
+++ libvts/kernel/scale.cl
@@ -9,8 +9,8 @@ KERNEL_DEF
  __kernel
  void limitf(__global float* to, __global const float* from,  const float top, const float bottom) {
    int x = get_global_id(0);
-   float v = from[x] + 1;
-   to[x] = v * sign(fmax(bottom,v) - bottom) * sign(top - fmin(top,v)) - 1;
-   /* v * (v >= a) * (v <= b) */
+   float v = from[x];
+   /*to[x] = v * sign(fmax(bottom,v) - bottom) * sign(top - fmin(top,v));*/
+   to[x] = v * isgreater(v,bottom) * isless(v,top); /* v * (v >= a) * (v <= b) */
  }
  )
--- libvts/test/api_test.cc
+++ libvts/test/api_test.cc
@@ -19,10 +19,12 @@
 
 constexpr int CastingTestSize = 13;
 
-extern char const cast_u8_f[] = "cast_u8_f";
-extern char const cast_f_u8[] = "cast_f_u8";
-extern char const scalef[]    = "scalef";
-extern char const limitf[]    = "limitf";
+extern char const cast_u8_f[]   = "cast_u8_f";
+extern char const cast_f_u8[]   = "cast_f_u8";
+extern char const scalef[]      = "scalef";
+extern char const limitf[]      = "limitf";
+extern char const edge_points[] = "edge_points";
+extern char const face_points[] = "face_points" ;
 
 
 // testing fixture and environment fixture
@@ -40,9 +42,6 @@ public:
 };
 
 testing::Environment* const global_env = testing::AddGlobalTestEnvironment(new ApiEnvironment);
-
-
-
 
 // Testing for test_cl_kernel_call
 
@@ -109,11 +108,10 @@ public:
   }
 };
 
-template<char const *kn, size_t ws>
+template<char const *kn>
 class KernelGen {
   const char * kernel_name;
   cl_kernel _kernel = 0;
-  size_t work_size = ws;
 public:
   KernelGen(size_t argc,...) {
     kernel_name = kn;
@@ -130,121 +128,21 @@ public:
   ~KernelGen() {
     clReleaseKernel(_kernel);
   }
-  cl_int callKernel() {
+  cl_int callKernel(cl_uint dim, const size_t *gws) {
     return clEnqueueNDRangeKernel(get_global_command_queue()[0],_kernel,
-                                  1,0,&work_size,0,0,0,0);
+                                  dim,0,gws,0,0,0,0);
   }
 };
 
-// Tesing for casting
-class CastingTest : public testing::Test{
-public:
-  MemoryGen<uint8_t> * ptr_u8      = nullptr;
-  MemoryGen<float>   * ptr_f       = nullptr;
-  MemoryGen<uint8_t> * out_u8      = nullptr;
-  MemoryGen<float>   * out_f       = nullptr;
-  uint8_t            * std_u8      = nullptr;
-  float              * std_f       = nullptr;
-
-  virtual void SetUp() {
-    try {
-      std_u8 = new uint8_t[CastingTestSize];
-      std_f  = new float[CastingTestSize];
-
-      for (auto i = 0; i < CastingTestSize; ++i) {
-        std_f[i] = i;
-        std_u8[i] = i;
-      }
-
-      ptr_f  = new MemoryGen<float>(CastingTestSize,
-                                    CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                                    std_f);
-      ptr_u8 = new MemoryGen<uint8_t>(CastingTestSize,
-                                      CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                                      std_u8);
-      out_f  = new MemoryGen<float>(CastingTestSize,
-                                    CL_MEM_WRITE_ONLY,
-                                    nullptr);
-      out_u8 = new MemoryGen<uint8_t>(CastingTestSize,
-                                      CL_MEM_WRITE_ONLY,
-                                      nullptr);
-      for (auto i = 0; i < 13; ++i) {
-        std_f[i]  = 64;
-        std_u8[i] = 64;
-      }
-    }
-    catch (std::exception e) {
-      if(ptr_f)  delete   ptr_f;
-      if(ptr_u8) delete   ptr_u8;
-      if(out_f)  delete   out_f;
-      if(out_u8) delete   out_u8;
-      if(std_u8) delete[] std_u8;
-      if(std_f)  delete[] std_f;
-      throw e;
-    }
-  }
-  virtual void TearDown() {
-    if(ptr_f)  delete   ptr_f;
-    if(ptr_u8) delete   ptr_u8;
-    if(out_f)  delete   out_f;
-    if(out_u8) delete   out_u8;
-    if(std_u8) delete[] std_u8;
-    if(std_f)  delete[] std_f;
-  }
-};
-
-TEST_F(CastingTest, case1) {
-  size_t work_size  = 13;
-  cl_int errCode;
-
-  auto cast8f_call =
-    KernelGen<cast_u8_f,CastingTestSize>
-    (2,
-     sizeof(cl_mem), out_f->getMemObj(),
-     sizeof(cl_mem), ptr_u8->getMemObj());
-
-  auto castf8_call =
-    KernelGen<cast_f_u8,CastingTestSize>
-    (2,
-     sizeof(cl_mem), out_u8->getMemObj(),
-     sizeof(cl_mem), ptr_f->getMemObj());
-
-
-
-  EXPECT_EQ(castf8_call.callKernel(), CL_SUCCESS);
-
-  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
-
-  EXPECT_EQ(out_u8->copyOut(std_u8), CL_SUCCESS);
-
-  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
-
-  EXPECT_EQ(cast8f_call.callKernel(), CL_SUCCESS);
-
-  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
-
-  EXPECT_EQ(out_f->copyOut(std_f), CL_SUCCESS);
-
-  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
-
-  for(auto i = 0; i < 13; ++i) {
-    EXPECT_EQ(std_u8[i],(uint8_t)i);
-  }
-
-  for (auto i = 0; i < 13; ++i) {
-	  EXPECT_EQ(std_f[i], (float)i);
-  }
-}
-
-
-template<typename T1, typename T2, int FxTestSize>
+// testing helper for y=f(x)
+template<typename T1, typename T2, int inSize, int outSize>
 class FxTest {
 public:
   FxTest() {
-    from = new MemoryGen<T1>(FxTestSize, CL_MEM_READ_WRITE, nullptr);
-    to   = new MemoryGen<T2>(FxTestSize, CL_MEM_READ_WRITE, nullptr);
-    origin = new T1[FxTestSize];
-    result = new T2[FxTestSize];
+    from = new MemoryGen<T1>(inSize,  CL_MEM_READ_WRITE, nullptr);
+    to   = new MemoryGen<T2>(outSize, CL_MEM_READ_WRITE, nullptr);
+    origin = new T1[inSize];
+    result = new T2[outSize];
   }
 
   ~FxTest() {
@@ -260,38 +158,323 @@ public:
   T2            * result = nullptr;
   void rands() {
     srand (time(NULL));
-    for(auto i = 0; i < FxTestSize; ++i){
+    for(auto i = 0; i < inSize; ++i){
       origin[i] = static_cast <T1> (rand()) / static_cast <T1> (RAND_MAX);
+    }
+  }
+  void map(std::function<T1(T1,int)> func){
+    srand (time(NULL));
+    for(auto i = 0; i < inSize; ++i){
+      origin[i] = func(origin[i],rand());
     }
   }
   cl_int write() {
     return clEnqueueWriteBuffer(get_global_command_queue()[0], *(from->getMemObj()), CL_TRUE,
-                                0, sizeof(T1)*FxTestSize, origin,
+                                0, sizeof(T1)*inSize, origin,
                                 0, 0, 0);
   }
   cl_int read() {
     return clEnqueueReadBuffer(get_global_command_queue()[0], *(to->getMemObj()), CL_TRUE,
-                               0, sizeof(T2)*FxTestSize, result,
+                               0, sizeof(T2)*outSize, result,
                                0, 0, 0);
   }
 };
 
-TEST(scale,scalef1) {
-  auto on = FxTest<float,float,13>();
+TEST(cast,u8f) {
+  auto on = FxTest<uint8_t,float,13,13>();
+  size_t ws = 13;
 
   on.rands();
   EXPECT_EQ(on.write(), CL_SUCCESS);
   float scal = 2;
   auto scalef_call =
-    KernelGen<scalef,13>
+    KernelGen<cast_u8_f>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(scalef_call.callKernel(1,&ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+  for(auto i = 0; i < 13; ++i){
+    EXPECT_EQ(on.result[i], (float)on.origin[i]);
+  }
+}
+
+
+TEST(cast,fu8) {
+  auto on = FxTest<float, uint8_t,13,13>();
+  size_t ws = 13;
+
+  on.rands();
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  float scal = 2;
+  auto scalef_call =
+    KernelGen<cast_f_u8>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(scalef_call.callKernel(1,&ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+  for(auto i = 0; i < 13; ++i){
+    EXPECT_EQ(on.result[i], (uint8_t)on.origin[i]);
+  }
+}
+
+
+TEST(scale,scalef1) {
+  auto on = FxTest<float,float,13,13>();
+  size_t ws = 13;
+
+  on.rands();
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  float scal = 2;
+  auto scalef_call =
+    KernelGen<scalef>
     (3,
      sizeof(cl_mem), on.to->getMemObj(),
      sizeof(cl_mem), on.from->getMemObj(),
      sizeof(float), &scal);
-  EXPECT_EQ(scalef_call.callKernel(), CL_SUCCESS);
+  EXPECT_EQ(scalef_call.callKernel(1,&ws), CL_SUCCESS);
   EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
   EXPECT_EQ(on.read(), CL_SUCCESS);
   for(auto i = 0; i < 13; ++i){
     EXPECT_EQ(on.result[i], scal * on.origin[i]);
   }
+}
+
+TEST(scale,limitf2) {
+  auto on = FxTest<float,float,13,13>();
+  size_t ws = 13;
+
+  on.rands();
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  float top    = 0.25;
+  float bottom = 0.75;
+  auto limitf_call =
+    KernelGen<limitf>
+    (4,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj(),
+     sizeof(float), &top,
+     sizeof(float), &bottom);
+  EXPECT_EQ(limitf_call.callKernel(1,&ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+  auto func = [&](float a){
+    if (a < top && a > bottom)
+      return a;
+    else
+      return (float)0;
+  };
+  for(auto i = 0; i < 13; ++i){
+    EXPECT_EQ(on.result[i], func(on.origin[i]));
+  }
+}
+
+
+
+float edgeList[] = {
+  0,  0,  0,  1,  0,  0,
+  1,  0,  0,  1,  1,  0,
+  1,  1,  0,  1,  1,  1,
+
+  1,  0,  0,  1,  0,  1,
+  1,  0,  1,  1,  1,  1,
+  1,  1,  1,  0,  1,  1,
+
+  1,  0,  1,  0,  0,  1,
+  0,  0,  1,  0,  1,  1,
+  0,  1,  1,  0,  1,  0,
+
+  0,  0,  1,  0,  0,  0,
+  0,  0,  0,  0,  1,  0,
+  0,  1,  0,  1,  1,  0,
+};
+
+TEST(draw,edge1) {
+  auto on = FxTest<float,float,1,1*72>();
+  size_t ws[] ={1,1,1};
+
+  on.rands();
+  on.map([](float a, int b){
+      std::cout << b % 2 << std::endl;
+      if(b % 2)
+        return a;
+      else
+        return (float)0;
+    });
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  auto called_kernel =
+    KernelGen<edge_points>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(called_kernel.callKernel(3,ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+
+  for(auto x = 0; x < 1; ++x)
+    for(auto y = 0; y < 1; ++y)
+      for(auto z = 0; z < 1; ++z) {
+        int i = x + y * 1 + z * 1 * 1;
+        float *cur = on.result + i * 72;
+        if (on.origin[i] > 0)
+          for (auto j = 0; j < 24; ++j){
+            EXPECT_EQ(cur[j*3+0], edgeList[j*3+0] + x);
+            EXPECT_EQ(cur[j*3+1], edgeList[j*3+1] + y);
+            EXPECT_EQ(cur[j*3+2], edgeList[j*3+2] + z);
+          }
+        else
+          for (auto j = 0; j < 72; ++j){
+            EXPECT_EQ(cur[j], 0);
+          }
+      }
+}
+
+
+
+TEST(draw,edge2) {
+  auto on = FxTest<float,float,8,8*72>();
+  size_t ws[] = {2,2,2};
+
+  on.rands();
+  on.map([](float a, int b){
+      std::cout << b % 2 << std::endl;
+      if(b % 2)
+        return a;
+      else
+        return (float)0;
+    });
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  auto called_kernel =
+    KernelGen<edge_points>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(called_kernel.callKernel(3,ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+
+  for(auto x = 0; x < 2; ++x)
+    for(auto y = 0; y < 2; ++y)
+      for(auto z = 0; z < 2; ++z) {
+        int i = x + y * 2 + z * 2 * 2;
+        float *cur = on.result + i * 72;
+        if (on.origin[i] > 0)
+          for (auto j = 0; j < 24; ++j){
+            EXPECT_EQ(cur[j*3+0], edgeList[j*3+0] + x);
+            EXPECT_EQ(cur[j*3+1], edgeList[j*3+1] + y);
+            EXPECT_EQ(cur[j*3+2], edgeList[j*3+2] + z);
+          }
+        else
+          for (auto j = 0; j < 72; ++j){
+            EXPECT_EQ(cur[j], 0);
+          }
+      }
+}
+
+
+
+
+const float faceList[] = {
+  0,  0,  0,  1,  0,  0,
+  1,  1,  0,  0,  1,  0,
+
+  0,  0,  0,  0,  1,  0,
+  0,  1,  1,  0,  0,  1,
+
+  0,  0,  0,  1,  0,  0,
+  1,  0,  1,  0,  0,  1,
+
+
+  1,  1,  1,  0,  1,  1,
+  0,  0,  1,  1,  0,  1,
+
+  1,  1,  1,  1,  0,  1,
+  1,  0,  0,  1,  1,  0,
+
+  1,  1,  1,  0,  1,  1,
+  0,  1,  0,  1,  1,  0,
+};
+
+TEST(draw,face1) {
+  auto on = FxTest<float,float,1,1*72>();
+  size_t ws[] ={1,1,1};
+
+  on.rands();
+  on.map([](float a, int b){
+      std::cout << b % 2 << std::endl;
+      if(b % 2)
+        return a;
+      else
+        return (float)0;
+    });
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  auto called_kernel =
+    KernelGen<face_points>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(called_kernel.callKernel(3,ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+
+  for(auto x = 0; x < 1; ++x)
+    for(auto y = 0; y < 1; ++y)
+      for(auto z = 0; z < 1; ++z) {
+        int i = x + y * 1 + z * 1 * 1;
+        float *cur = on.result + i * 72;
+        if (on.origin[i] > 0)
+          for (auto j = 0; j < 24; ++j){
+            EXPECT_EQ(cur[j*3+0], faceList[j*3+0] + x);
+            EXPECT_EQ(cur[j*3+1], faceList[j*3+1] + y);
+            EXPECT_EQ(cur[j*3+2], faceList[j*3+2] + z);
+          }
+        else
+          for (auto j = 0; j < 72; ++j){
+            EXPECT_EQ(cur[j], 0);
+          }
+      }
+}
+
+
+TEST(draw,face2) {
+  auto on = FxTest<float,float,8,8*72>();
+  size_t ws[] = {2,2,2};
+
+  on.rands();
+  on.map([](float a, int b){
+      std::cout << b % 2 << std::endl;
+      if(b % 2)
+        return a;
+      else
+        return (float)0;
+    });
+  EXPECT_EQ(on.write(), CL_SUCCESS);
+  auto called_kernel =
+    KernelGen<face_points>
+    (2,
+     sizeof(cl_mem), on.to->getMemObj(),
+     sizeof(cl_mem), on.from->getMemObj());
+  EXPECT_EQ(called_kernel.callKernel(3,ws), CL_SUCCESS);
+  EXPECT_EQ(clFinish(get_global_command_queue()[0]),CL_SUCCESS);
+  EXPECT_EQ(on.read(), CL_SUCCESS);
+  
+  for(auto x = 0; x < 2; ++x)
+    for(auto y = 0; y < 2; ++y)
+      for(auto z = 0; z < 2; ++z) {
+        int i = x + y * 2 + z * 2 * 2;
+        float *cur = on.result + i * 72;
+        if (on.origin[i] > 0)
+          for (auto j = 0; j < 24; ++j){
+            EXPECT_EQ(cur[j*3+0], faceList[j*3+0] + x);
+            EXPECT_EQ(cur[j*3+1], faceList[j*3+1] + y);
+            EXPECT_EQ(cur[j*3+2], faceList[j*3+2] + z);
+          }
+        else
+          for (auto j = 0; j < 72; ++j){
+            EXPECT_EQ(cur[j], 0);
+          }
+      }
 }
